8814d54b3e102e86e8d373d11b9ce1fb0f098421
dmartinezsaa <dani@dani-GE60-2QD>
2017-11-14 08:41:03 +0100

    Inicio programa

--- boletin9_2/src/boletin9_2/Calculos.java
+++ boletin9_2/src/boletin9_2/Calculos.java
@@ -14,30 +14,16 @@ import java.util.Scanner;
 public class Calculos {
     Scanner scan=new Scanner(System.in);
     int num=0, suma=0;
-    byte f=1;
-    float producto=1;
-    String seguir;
+    double producto=1;
     public void calcular(){
-    do{     
-            System.out.println("Inserte número:");
-            num=scan.nextInt();
-            suma=suma+num;
-            producto=producto*num;
-            
-            System.out.println("Desea seguir?");
-            seguir=scan.next();
-            switch(seguir){
-                case "si": f=0;
-                break;
-                case "no": f=1;
-                break;
-                default: System.out.println("Error");
-                break;
-            }
-            
-    }while(f==0);
-
-    System.out.println("Suma:"+suma+"\nProducto:"+producto);
+        
+        
+        for(byte i=10;i<=90;i++){
+            suma=suma+i;
+            producto=i*producto;
+        }
+        System.out.println("Suma: "+suma+"\nProducto: "+producto);
+        
     
     }
 }
